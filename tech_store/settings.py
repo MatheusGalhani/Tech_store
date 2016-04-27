0658e7c32cba36dbc932d0249443d2aa3115180a
Matheus <matheusgalhani@hotmail.com>
2016-04-27 10:47:57 -0300

    Change image into Produto

--- tech_store/settings.py
+++ tech_store/settings.py
@@ -102,4 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'tech_store/static_cnd')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'tech_store/media_cnd')
+
