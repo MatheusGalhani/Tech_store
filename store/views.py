5b49f8478b4f075e7d1d21c5f33c23c011b3a6b4
Matheus <matheusgalhani@hotmail.com>
2016-04-28 09:44:47 -0300

    Add template to incial page

--- store/views.py
+++ store/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Produto
 # Create your views here.
 def post_list(request):
-    return render(request, 'store/post_list.html', {})
+	posts = Produto.objects.order_by('id_produto')
+	return render(request, 'store/post_list.html', {'posts': posts})
+
--- tech_store/settings.py
+++ tech_store/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'tech_store/static_cnd')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'tech_store/store/static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'tech_store/media_cnd')
 
