82684334d06a06057c4abe0d86f810052a462250
Matheus <matheusgalhani@hotmail.com>
2016-05-12 08:17:44 -0300

    Alter view Reset

--- store/views.py
+++ store/views.py
@@ -43,18 +43,4 @@ def Buy(request):
     return render(request, "store/store.html", {'posts': posts})
 
 def Reset(request):
-    if request.method == "POST":
-        subject = request.POST.get('subject', '')
-        message = request.POST.get('message', '')
-        from_email = request.POST.get('from_email', '')
-        return HttpResponse(from_email)
-        if subject and message and from_email:
-            try:
-                send_mail(subject, message, from_email, ['admin@example.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return HttpResponseRedirect('/contact/thanks/')
-        else:
-            return HttpResponse('Make sure all fields are entered and valid.')
-
     return render(request, "store/reset_password.html", {})
